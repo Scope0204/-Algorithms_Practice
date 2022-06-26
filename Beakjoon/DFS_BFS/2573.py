ed008336de92c124cb827b05fe629ce867f26dd4
Scope0204 <scope0204@gmail.com>
2022-06-26 15:14:04 +0900

    BFS_런타임해결

--- Beakjoon/DFS_BFS/2573.py
+++ Beakjoon/DFS_BFS/2573.py
@@ -1,3 +1,5 @@
+from collections import deque
+'''
 # 1. 1년 후 빙산 높이
 def next_year():
     global graph,n,m
@@ -51,9 +53,8 @@ def dfs(x,y,visited):
             if graph[nx][ny]: # 값이 있는 경우 = 이어진 하나의 빙산
                 dfs(nx,ny,visited) # 해당 위치에서 동서남북 체크
 
-
 n,m = map(int,input().split()) #행,열
-graph = [ list(map(int,input().split())) for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
 iceberg = 1 # 처음 빙산의 갯수는 항상 1 
 answer = 0
 
@@ -66,4 +67,81 @@ while iceberg<2:
     next_ice() # 빙산 갯수 측정
 
 print(answer)
+'''
 
+#로직 
+def bfs():
+    q = deque()
+    q.append(iceberg[0])
+    select_iceberg = 0 # 조회한 빙산의 갯수
+    dx = [0,0,1,-1] # 동서남북 - 오,왼,아래,위
+    dy = [1,-1,0,0]
+    melting=[] # 1년후, 녹을 빙산의 좌표값과 녹는 높이
+
+
+    visited = [[False]*m for _ in range(n)]
+    visited[iceberg[0][0]][iceberg[0][1]] = True # 시작점 빙산
+
+    while q:
+        x,y = q.popleft()
+
+        select_iceberg += 1  
+        count = 0 # 녹는 높이
+
+        # 1. 주변 바다 탐색(0인 값)
+        for i in range(4):
+            nx = dx[i]+x
+            ny = dy[i]+y
+            if nx >= 0 and ny >= 0:
+                if graph[nx][ny] == 0: #주변이 바다 -> 갯수만큼 녹음
+                    count += 1
+                elif graph[nx][ny] and not visited[nx][ny]: #빙산인 경우 and 아직 방문하지않음
+                    visited[nx][ny] = True
+                    q.append((nx,ny))
+        
+        if count != 0 : # 녹는 빙산이라면(= 주변에 바다가 있다면)
+            melting.append((x,y,count)) # 해당 빙산의 좌표와 녹는점 
+
+    #2. 0의 갯수만큼 빙산이 녹음(단, 0인경우 더 이상 녹지 않는다)
+    for x,y,cnt in melting:
+        graph[x][y] = graph[x][y] - cnt if graph[x][y]-cnt > 0 else 0  
+        
+        #다 녹은 빙산은  bfs 체크리스트에서 제외 
+        if graph[x][y] == 0 and (x,y) in iceberg: 
+            iceberg.remove((x,y)) 
+        
+    # 조회한 빙산의 갯수
+    return select_iceberg
+
+
+#입력
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+#풀이
+year = 0
+iceberg = []
+
+for x in range(n):
+    for y in range(m):
+        if graph[x][y]:
+            iceberg.append((x,y)) # 빙산이 있는 위치 값 추가
+
+while 1:
+    # 선택된 빙산의 갯수가 현재 빙산의 갯수와 다르다 -> 쪼개졌다 
+    if len(iceberg) == 0 : 
+        year = 0 
+        break
+    
+    if len(iceberg) != bfs():
+        break
+
+    year += 1
+
+    # # 전부 다 녹은 경우 = 그래프의 모든 값의 합이 0인 경우 
+    # if sum(map(sum, graph[1:-1])) == 0: 
+    #     year = 0
+    #     break
+
+#출력
+print(year)
